a357d3cf2a690bbfb8513d097296147c10457222
YounessNB94 <younessnb@hotmail.fr>
2025-12-07 11:56:08 +0100

    init front

---
+++ front/EcodeliFront/src/app/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Ecodeli Front is running
+      </p>
+    </>
+  )
+}
+
+export default App
